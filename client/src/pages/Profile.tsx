0ef1ed6d6d9e5190555c4cb4c1e74bf64c9978e2
SuperShreejit <assassinshreejitpal@gmail.com>
2022-09-03 15:37:26 +0530

    timeline + timeline scss

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -2,17 +2,25 @@ import ProfileHead from '../components/ProfileHead'
 import SectionTitle from '../components/SectionTitle'
 import UserPosts from '../components/UserPosts'
 import { SECTION_TITLE } from '../constants/component'
-import { PAGE_CONTAINER_CLASS } from '../constants/pages'
+import { DARK_CLASS, PAGE_CONTAINER_CLASS } from '../constants/pages'
 import useAuth from '../hooks/useAuth'
 import useQueryData from '../hooks/useQueryData'
+import useTheme from '../hooks/useTheme'
 import '../sass/pages/_profile.scss'
 
 const Profile = () => {
 	const { user, userId } = useAuth()
 	const { profile } = useQueryData()
+	const { isDark } = useTheme()
 
 	return (
-		<main className={PAGE_CONTAINER_CLASS.PROFILE}>
+		<main
+			className={
+				isDark
+					? `${PAGE_CONTAINER_CLASS.PROFILE} ${DARK_CLASS}`
+					: `${PAGE_CONTAINER_CLASS.PROFILE}`
+			}
+		>
 			<ProfileHead user={{ user: user ? user : profile[0][1].user }} />
 			<SectionTitle title={SECTION_TITLE.POSTS} />
 			<UserPosts userId={userId} />
--- client/src/pages/TimeLine.tsx
+++ client/src/pages/TimeLine.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import FormAlert from '../components/FormAlert'
 import Post from '../components/Post'
-import { PAGE_CONTAINER_CLASS } from '../constants/pages'
+import { PAGE_CONTAINER_CLASS, DARK_CLASS } from '../constants/pages'
 import getError from '../helpers/getError'
+import useTheme from '../hooks/useTheme'
 import useTimeline from '../hooks/useTimeline'
 import Loading from './../components/Loading'
 
 const TimeLine = () => {
 	const { data, error, isError, isLoading, isSuccess } = useTimeline()
+	const { isDark } = useTheme()
 
 	if (isLoading)
 		return (
@@ -32,7 +33,13 @@ const TimeLine = () => {
 		)
 
 	return (
-		<main className={PAGE_CONTAINER_CLASS.TIMELINE}>
+		<main
+			className={
+				isDark
+					? `${PAGE_CONTAINER_CLASS.TIMELINE} ${DARK_CLASS}`
+					: `${PAGE_CONTAINER_CLASS.TIMELINE}`
+			}
+		>
 			{isSuccess &&
 				typeof data !== 'string' &&
 				data?.data.success &&
