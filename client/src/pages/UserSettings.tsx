ac5f892bea529209b74ff585a5696d2f475b9258
SuperShreejit <assassinshreejitpal@gmail.com>
2022-10-06 17:39:52 +0530

    fixes

--- client/src/pages/UserSettings.tsx
+++ client/src/pages/UserSettings.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { DARK_CLASS, PAGE_CONTAINER_CLASS } from '../constants/pages'
+import useAuth from '../hooks/useAuth'
+import useTheme from '../hooks/useTheme'
 
 const UserSettings = () => {
-  return (
-    <div>UserSettings</div>
-  )
+	const { isDark } = useTheme()
+	const { user, userId } = useAuth()
+
+	return (
+		<main
+			className={
+				isDark
+					? `${PAGE_CONTAINER_CLASS.SETTINGS} ${DARK_CLASS}`
+					: `${PAGE_CONTAINER_CLASS.SETTINGS}`
+			}
+		>
+			UserSettings
+		</main>
+	)
 }
 
 export default UserSettings
